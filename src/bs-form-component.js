6817af7ee17c9340cb79a76b6ba5b6b1ee6db9bb
James Ngo <James.Ngo@toronto.ca>
2020-07-07 16:34:54 -0400

    Refactor

--- src/bs-form-component.js
+++ src/bs-form-component.js
@@ -7,12 +7,12 @@ const BsFormComponent = Object.assign({}, FormComponent, {
 			<h1 v-if="title">{{title}}</h1>
 			<component v-for="section in sections" v-bind="section" v-bind:data="data"></component>
 			<p>
-				<template v-for="({ label = 'Submit', ...button }, index) in buttons">
+				<template v-for="({ label = 'Submit', type, ...button }, index) in buttons">
 					<button
 						class="btn"
 						v-bind="button"
 						v-bind:class="Object.assign({'btn-primary': index === 0, 'btn-secondary': index !== 0}, button.class)"
-						v-bind:type="button.type !== undefined ? button.type : (index === 0 ? null : 'button')"
+						v-bind:type="type !== undefined ? type : index === 0 ? null : 'button'"
 					>{{label}}</button>{{ index < buttons.length -1 ? ' ': '' }}
 				</template>
 			</p>
